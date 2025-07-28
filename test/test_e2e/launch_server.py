3453d0813948d4b3edc470a7e7257c596a23d7da
ginkolove <1396270255@qq.com>
2025-07-28 17:34:10 +0800

    修复启动sever时提交lora数量冗余的情况

--- test/test_e2e/launch_server.py
+++ test/test_e2e/launch_server.py
@@ -29,9 +29,14 @@ if __name__ == "__main__":
     cmd += f" --pool-size-lora {args.pool_size_lora}"
 
     num_iter = args.num_adapter // len(adapter_dirs) + 1
+    input_adapter_dirs=[]
     for i in range(num_iter):
         for adapter_dir in adapter_dirs:
-            cmd += f" --lora {adapter_dir}-{i}"
+                input_adapter_dirs.append(f"{adapter_dir}-{i}")
+        input_adapter_dirs = input_adapter_dirs[:args.num_adapter]
+    print(f"Using {len(input_adapter_dirs)} adapters: {input_adapter_dirs}")
+    for adapter_dir in input_adapter_dirs:
+        cmd += f" --lora {adapter_dir}"
 
     cmd += " --swap"
     # cmd += " --scheduler pets"
